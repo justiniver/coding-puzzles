f15e4214c81084fe37f31c3a944458f80430c001
Justin Iverson <56553705+justiniver@users.noreply.github.com>
2026-05-12 23:16:50 -0400

    Add: combinations-of-a-phone-number - submission-2

---
+++ Data Structures & Algorithms/combinations-of-a-phone-number/submission-2.py
@@ -0,0 +1,32 @@
+class Solution:
+    def letterCombinations(self, digits: str) -> List[str]:
+        if not digits:
+            return []
+            
+        dig_map = {
+            "2": "abc",
+            "3": "def",
+            "4": "ghi",
+            "5": "jkl",
+            "6": "mno",
+            "7": "pqrs",
+            "8": "tuv",
+            "9": "wxyz",
+        }
+
+        out = []
+        comb = []
+        def create_comb_from_ith(i: int):
+            if i >= len(digits):
+                out.append("".join(comb))
+                return
+            
+            curr_dig = digits[i]
+            for letter in dig_map[curr_dig]:
+                comb.append(letter)
+                create_comb_from_ith(i + 1)
+                comb.pop()
+        
+        create_comb_from_ith(0)
+        return out
+        
