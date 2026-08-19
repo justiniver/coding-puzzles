43ff322e84c7f9bda18ecd754dd2d13fa0cdd87e
Justin Iverson <56553705+justiniver@users.noreply.github.com>
2026-08-19 10:02:08 +0900

    Add: string-encode-and-decode - submission-2

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-2.py
@@ -0,0 +1,9 @@
+class Solution:
+
+    def encode(self, strs: List[str]) -> str:
+        return " ".join(strs)
+
+    def decode(self, s: str) -> List[str]:
+        if not s:
+            return [""]
+        return s.split(" ")
